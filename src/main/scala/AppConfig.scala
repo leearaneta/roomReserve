170f0ebddea4165b67daae82ac770492b4c7dc44
leearaneta <lee.araneta@gmail.com>
2017-11-29 13:21:40 -0500

    refactored configuration

--- src/main/scala/AppConfig.scala
+++ src/main/scala/AppConfig.scala
@@ -7,15 +7,12 @@ object AppConfig {
   case class DeviceConfig(username: String, password: String, deviceID: String)
   case class Configuration(accessToken: String, deviceList: List[DeviceConfig])
 
-  private def getConfigFromYAML(filepath: String) = {
-    val reader = new FileReader(filepath)
+  private val configuration: Configuration = {
+    val reader = new FileReader("../yaml_stuff/configuration.yaml") // maybe have this as argument
     val mapper = new ObjectMapper(new YAMLFactory())
     mapper.registerModule(DefaultScalaModule)
     mapper.readValue(reader, classOf[Configuration])
   }
-
-  private val yamlPath = "../yaml_stuff/configuration.yaml" // maybe input this as argument
-  private val configuration: Configuration = getConfigFromYAML(yamlPath)
 
   val deviceList: List[DeviceConfig] = configuration.deviceList
   val accessToken: String = configuration.accessToken
