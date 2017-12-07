001a8f3abbb9b4fa733305cc7823d1e83fe4b9f1
leearaneta <lee.araneta@gmail.com>
2017-12-06 21:58:45 -0500

    extends app

--- src/main/scala/Flash.scala
+++ src/main/scala/Flash.scala
@@ -1,8 +1,6 @@
-object Flash {
+object Flash extends App {
   import AppConfig._
   import sys.process._
-  def main(args: Array[String]): Unit = {
-    def flash(x: DeviceConfig): Unit = {s"particle flash ${x.deviceID} ../particle_binaries/roomReserve.bin" !}
-    deviceList.foreach(flash)
-  }
+  def flash(x: DeviceConfig): Unit = {s"particle flash ${x.deviceID} ../particle_binaries/roomReserve.bin" !}
+  deviceList.foreach(flash)
 }
